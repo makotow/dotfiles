6dac03b17d49baa946072bee2e49fe29a29c9cf0
makotow <makoto.wata@gmail.com>
2012-08-03 08:43:20 +0900

    Modified os specific code.

---
+++ auto-install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# set install directory
+DOTFILES = $1
+
+if[ $1 == '' ]; then
+    $1 = "."
+end
+
+#20120803 working
+cd "$DOTFILES"
+git clone https://github.com:makotow/dotfiles.git "$DOTFILES"
+git submodule update --init
+
+./setup.rb
