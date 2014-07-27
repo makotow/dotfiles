0b3f2c9e1ed3040fa95d888d33ca0daa1e5535e5
makotow <makoto.wata@gmail.com>
2014-07-27 19:20:54 +0900

    snapshot

---
+++ hydra/init.lua
@@ -0,0 +1,75 @@
+-- refers to grid.lua in this directory, taken from the Hydra wiki: https://github.com/sdegutis/hydra/wiki/Useful-Hydra-libraries
+dofile(package.searchpath("grid", package.path))
+
+hydra.alert "Hydra, at your service."
+
+pathwatcher.new(os.getenv("HOME") .. "/.hydra/", hydra.reload):start()
+hydra.autolaunch.set(true)
+
+hydra.menu.show(function()
+    local updatetitles={[true] = "Install Update", [false] = "Check for Update..."}
+    local updatefns = {[true] = updates.install, [false] = checkforupdates}
+    local hasupdate = (updates.newversion~=nil)
+
+    return {
+      {tilte = "Reload Config", fn = hydra.reload},
+      {tilte = "Open REPL", fn = repl.open},
+      {title = "-"},
+      {title = "About Hydra", fn = hydra.showabout},
+      {tilte = updatetitles[hasupdate], fn = updatefns[hasupdate]},
+      {title = "Quit", fn = os.exit},
+    }
+end)
+
+local mash = {"cmd", "alt", "ctrl"}
+local mashshift = {"cmd", "alt", "shift"}
+
+local function opendictionary()
+  hydra.alert("Lexicon, at your service.", 0.75)
+  application.launchorfocus("Dictionary")
+end
+
+hotkey.bind(mash, 'D', opendictionary)
+
+hotkey.bind(mash, ';', function() ext.grid.snap(window.focusedwindow()) end)
+hotkey.bind(mash, "'", function() fnutils.map(window.visiblewindows(), ext.grid.snap) end)
+
+hotkey.bind(mash, '=', function() ext.grid.adjustwidth( 1) end)
+hotkey.bind(mash, '-', function() ext.grid.adjustwidth(-1) end)
+
+hotkey.bind(mashshift, 'H', function() window.focusedwindow():focuswindow_west() end)
+hotkey.bind(mashshift, 'L', function() window.focusedwindow():focuswindow_east() end)
+hotkey.bind(mashshift, 'K', function() window.focusedwindow():focuswindow_north() end)
+hotkey.bind(mashshift, 'J', function() window.focusedwindow():focuswindow_south() end)
+
+hotkey.bind(mash, 'M', ext.grid.maximize_window)
+
+-- hotkey.bind(mash, 'N', ext.grid.pushwindow_nextscreen)
+-- hotkey.bind(mash, 'P', ext.grid.pushwindow_prevscreen)
+
+hotkey.bind(mash, 'J', ext.grid.pushwindow_down)
+hotkey.bind(mash, 'K', ext.grid.pushwindow_up)
+hotkey.bind(mash, 'H', ext.grid.pushwindow_left)
+hotkey.bind(mash, 'L', ext.grid.pushwindow_right)
+
+hotkey.bind(mash, 'U', ext.grid.resizewindow_taller)
+hotkey.bind(mash, 'Y', ext.grid.resizewindow_shorter)
+hotkey.bind(mash, 'O', ext.grid.resizewindow_wider)
+hotkey.bind(mash, 'I', ext.grid.resizewindow_thinner)
+
+hotkey.bind(mash, 'X', logger.show)
+hotkey.bind(mash, "R", repl.open)
+
+function checkforupdates()
+  -- I'm fine with making this a global; then I can call it in the REPL if I want.
+  hydra.updates.check(function(hasone)
+      if hasone then
+        notify.show("Hydra update available", "Go download it!", "Click here to see the release notes.", "hasupdate")
+      end
+  end)
+end
+notify.register("hasupdate", function() os.execute("open " .. updates.changelogurl) end)
+
+checkforupdates()
+
+timer.new(timer.days(1), checkforupdates):start()
